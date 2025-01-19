5710ab0030674fa275484b095cd28447d1fa3b82
Florian Rey <nervo@nervo.net>
2025-01-20 00:34:17 +0100

    switch between model state with "s" key

--- model/model.go
+++ model/model.go
@@ -7,38 +7,61 @@ import (
 	tea "github.com/charmbracelet/bubbletea/v2"
 )
 
+type State int
+
+const (
+	StateIntro State = iota
+	StateGame
+)
+
+var models = map[State]engine.Model{
+	StateIntro: intro.New(),
+	StateGame:  game.New(),
+}
+
 func New() *Model {
-	return &Model{
-		intro: intro.New(),
-		game:  game.New(),
-	}
+	return &Model{}
 }
 
 type Model struct {
-	model engine.Model
-	intro engine.Model
-	game  engine.Model
+	state State
 }
 
 func (m *Model) Init() {
-	m.intro.Init()
-	m.game.Init()
-
-	m.model = m.game
+	m.state = StateGame
+	models[m.state].Init()
 }
 
 func (m *Model) Update(msgs []tea.Msg) {
-	m.model.Update(msgs)
+	// Messages
+	for _, msg := range msgs {
+		switch msg := msg.(type) {
+		case tea.KeyPressMsg:
+			switch msg.String() {
+			case "s":
+				switch m.state {
+				case StateIntro:
+					m.state = StateGame
+				case StateGame:
+					m.state = StateIntro
+				}
+				models[m.state].Init()
+				return
+			}
+		}
+	}
+
+	models[m.state].Update(msgs)
 }
 
 func (m *Model) Bodies() (bodies engine.Bodies) {
-	return m.model.Bodies()
+	return models[m.state].Bodies()
 }
 
 func (m *Model) Sprites8() (sprites engine.Sprites8) {
-	return m.model.Sprites8()
+	return models[m.state].Sprites8()
 }
 
 func (m *Model) Sprites24() (sprites engine.Sprites24) {
-	return m.model.Sprites24()
+	return models[m.state].Sprites24()
 }
